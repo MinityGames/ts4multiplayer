abbdfc4c5e75aa7baf875d6af2763e263a8ee698
thepancake1 <theojabalde@gmail.com>
2018-05-29 22:25:31 +0800

    Reload function now works by recursively scanning directories until it finds hte module you wanted to reload instead of having to type in the whole directory

--- Scripts/ts4mp/debug/reload.py
+++ Scripts/ts4mp/debug/reload.py
@@ -5,22 +5,30 @@ import sims4.commands
 from sims4 import reload
 from ts4mp.core.mp_utils import get_sims_documents_directory
 
-#use it like ts4mp.reload Mods\ts4multiplayer\Scripts\ts4mp\core\overrides\system_distributor
+#use it like Scripts\ts4mp\debug\reload
 @sims4.commands.Command('ts4mp.reload', command_type=sims4.commands.CommandType.Live)
 def reload_maslow(module: str, _connection=None):
     output = sims4.commands.CheatOutput(_connection)
 
     try:
         dirname = get_sims_documents_directory() + "Mods/ts4multiplayer"
-        #filename = os.path.join(dirname, module) + ".py"
+        filename = os.path.join(dirname, module) + ".py"
+        reloaded_module = reload.reload_file(filename)
+        output("Reloading {}".format(filename))
 
-        output("Reloading {}".format(module))
+
+
+
         for root, dirnames, filenames in os.walk(dirname):
             for a in range(0, len(filenames)):
                 if filenames[a].split("\n")[-1] == module + ".py":
-                    # output(filenames[a])
                     filename = filenames[a]
-        reloaded_module = reload.reload_file(filename)
+                    output(os.path.join(root, filename))
+
+                    reloaded_module = reload.reload_file(os.path.join(root, filename))
+                    output("Reloading {}".format(filename))
+
+
 
         if reloaded_module is not None:
             output("Done reloading!")
