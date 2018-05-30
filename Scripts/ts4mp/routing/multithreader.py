36024c7e642e5edaaff41ccaca5ddd5b25567df4
thepancake1 <theojabalde@gmail.com>
2018-05-31 01:08:22 +0800

    stuff
    stuff

--- Scripts/ts4mp/routing/multithreader.py
+++ Scripts/ts4mp/routing/multithreader.py
@@ -30,7 +30,7 @@ def show_notif2(time):
 
 
 def show_notif3(time):
-    notification = UiDialogNotification.TunableFactory().default(services.get_active_sim(), text=lambda **_: LocalizationHelperTuning.get_raw_text("Autonomy time: {0:.4} s".format(time)))
+    notification = UiDialogNotification.TunableFactory().default(services.get_active_sim(), text=lambda **_: LocalizationHelperTuning.get_raw_text("Autonomy time: {0:.2} s".format(time)))
     notification.show_dialog()
 total_time = 0
 def archive_plan(planner, path, ticks, time):
@@ -74,12 +74,10 @@ class Timer():
         if (self.t2 - self.t1) * 1000 > 20:
             if self.name == "Transition Sequence":
                 show_notif2(total_p_time)
-                
+
         if self.name == "Autonomy":
-                show_notif3(self.t2 - self.t1) 
-                
-        if (self.t2 - self.t1) * 1000 > 20:
-            ts4mp_log(self.name, "time: ,{0:.2f}".format(self.t2 - self.t1))
+                show_notif3( (self.t2 - self.t1) )
+        ts4mp_log(self.name, "time: {}".format((self.t2 - self.t1) * 1000))
 
 import _pathing
 def generate_path(self, timeline):
@@ -139,7 +137,7 @@ def generate_path(self, timeline):
 
                 except Exception as e:
                     ts4mp_log("errors", str(e))
-     
+
 
             new_route = routing.Route(self.route.origin, self.route.goals, additional_origins=self.route.origins, routing_context=self.route.context)
             new_route.path.copy(self.route.path)
@@ -215,7 +213,7 @@ def routing_debug_follow( _connection=None):
             return False
         pos = services.get_active_sim().position
         x = pos.x + 5
-        y = pos.y 
+        y = pos.y
         z = pos.z
 
         def _do_route_gen(timeline):
@@ -444,7 +442,7 @@ def _estimate_distance(self, ignore_all_other_sis):
             if ignore_all_other_sis:
                 self.transition = None
         return result
-        
+
 def estimate_final_path_distance(self, timeline, ignore_all_other_sis):
     with Timer("Transition Sequence"):
         with AutonomyAffordanceTimes.profile_section(AutonomyAffordanceTimes.AutonomyAffordanceTimesType.TRANSITION_SEQUENCE):
@@ -470,9 +468,9 @@ import autonomy.autonomy_modes
 log = ""
 trace_count = 0
 def tracefunc(frame, event, arg, indent=[0]):
-    global log 
+    global log
     global trace_count
-    
+
 
     if event == "call":
         indent[0] += 2
@@ -483,10 +481,10 @@ def tracefunc(frame, event, arg, indent=[0]):
         log += to_print + "\n"
 
         indent[0] -= 2
-        
+
     if trace_count % 100000 == 0:
         ts4mp_log("time", log)
-        
+
     return tracefunc
 
 import sys
@@ -515,229 +513,5 @@ def _update_gen(self, timeline):
             yield timeline.run_child(sleep_element)
             #sys.setprofile(None)
 
-def run_stuff(self, generat):
-    for thing in generat:
-        ts4mp_log("Modes", thing)
 
-        yield thing 
-
-
-            
-            
-def run_gen(self, timeline, timeslice):
-
-    self._motive_scores = self._score_motives()
-    #ts4mp_log("Modes", str(self._motive_scores))
-    generat = self._run_gen(timeline, timeslice)
-    ts4mp_log("Modes", type(self))
-    with Timer("Modes"):
-        pass
-        # for value in generat:
-            # ts4mp_log("Modes", value)
-
-    #result = yield from self._run_gen(timeline, timeslice)
-    #result = yield from run_stuff(self, generat)
-    result = yield from self._run_gen(timeline, timeslice)
-
-    return result
-from _weakrefset import WeakSet
-  
-def _dont_timeslice_gen(timeline):
-    return False
-
-    yield None
-    
-    
-
-from autonomy.autonomy_interaction_priority import AutonomyInteractionPriority
-import itertools
-import date_and_time
-import random
-from autonomy.autonomy_modes import AutonomyMode
-timeslice_logger = sims4.log.Logger('AutonomyTimeslice', default_owner='rez')
-def block_until_done(timeline):
-    return False
-import random
-def _run_gen(self, timeline, timeslice):
-    try:
-        #ts4mp_log("potential", "start")
-
-        if self._should_log(self._sim):
-            logger.debug('Processing {}', self._sim)
-        gsi_enabled_at_start = gsi_handlers.autonomy_handlers.archiver.enabled
-        if gsi_enabled_at_start:
-            if self._gsi_objects is None:
-                self._gsi_objects = []
-            if self._gsi_interactions is None:
-                self._gsi_interactions = []
-        self._actively_scored_motives, motives_to_score = self._get_motives_to_score()
-        if not self._actively_scored_motives and not self._request.object_list:
-            return
-        else:
-            self._motives_being_solved = self._get_all_motives_currently_being_solved()
-            if timeslice is None:
-                timeslice_if_needed_gen = _dont_timeslice_gen
-            else:
-                start_time = time.clock()
-
-                def timeslice_if_needed_gen(timeline):
-                    nonlocal start_time
-                    time_now = time.clock()
-                    elapsed_time = time_now - start_time
-                    if elapsed_time < timeslice:
-                        return False
-                    if self._timestamp_when_timeslicing_was_removed is not None:
-                        enable_long_slice = False
-                    else:
-                        total_elapsed_time = time_now - self._process_start_time
-                        if total_elapsed_time > self.MAX_REAL_SECONDS_UNTIL_TIMESLICING_IS_REMOVED:
-                            timeslice_logger.debug('Autonomy request for {} took too long; timeslicing is removed.', self._sim)
-                            self._timestamp_when_timeslicing_was_removed = time_now
-                            enable_long_slice = False
-                        else:
-                            enable_long_slice = True
-                        start_time_now = time.time()
-                        if enable_long_slice:
-                            sleep_element = element_utils.sleep_until_next_tick_element()
-                        else:
-                            sleep_element = elements.SleepElement(date_and_time.TimeSpan(0))
-                        yield timeline.run_child(sleep_element)
-                        self._request.on_end_of_time_slicing(start_time_now)
-                        if self._sim is None or not self._request.valid:
-                            self._clean_up()
-                            raise autonomy.autonomy_exceptions.AutonomyExitException()
-                        start_time = time.clock()
-                        return True
-
-            best_threshold = None
-            while True:
-                self._inventory_posture_score_cache = {}
-                objects_to_score = self._request.objects_to_score_gen(self._actively_scored_motives)
-                ts4mp_log("stuff", "Objects to score: {}".format(len(objects_to_score)))
-                #to_sample = min(len(objects_to_score), 10)
-                #objects_to_score = WeakSet(random.sample(objects_to_score, to_sample))
-                objects_to_score = WeakSet(objects_to_score)
-               # objects_to_score = WeakSet(random.sample(self._request.objects_to_score_gen(self._actively_scored_motives, 5))
-                ts4mp_log("stuff", "Objects to score: {}".format(len(objects_to_score)))
-                while 1:
-                    yield from timeslice_if_needed_gen(timeline)
-                    try:
-                        obj = objects_to_score.pop()
-                    except KeyError:
-                        break
-                    with Timer("Score Object Interactions"):
-                        object_result, best_threshold = yield from self._score_object_interactions_gen(timeline, obj, timeslice_if_needed_gen, None, best_threshold)
-                        if self._gsi_objects is not None:
-                            self._gsi_objects.append(object_result.get_log_data())
-                        if not obj.is_sim:
-                            inventory_component = obj.inventory_component
-                            if inventory_component and inventory_component.should_score_contained_objects_for_autonomy and inventory_component.inventory_type not in self._inventory_posture_score_cache:
-                                best_threshold = yield from self._score_object_inventory_gen(timeline, inventory_component, timeslice_if_needed_gen, best_threshold)
-                            else:
-                                continue
-
-                for aop_list in self._limited_affordances.values():
-                    valid_aop_list = [aop_data for aop_data in aop_list if aop_data.aop.target is not None]
-                    ts4mp_log("stuff", "Aops to score: {}".format(len(valid_aop_list)))
-
-                    num_aops = len(valid_aop_list)
-                    if num_aops > self.NUMBER_OF_DUPLICATE_AFFORDANCE_TAGS_TO_SCORE:
-                        final_aop_list = random.sample(valid_aop_list, self.NUMBER_OF_DUPLICATE_AFFORDANCE_TAGS_TO_SCORE)
-                    else:
-                        final_aop_list = valid_aop_list
-                        
-                    with Timer("Score AOPs"):
-
-                        for aop_data in final_aop_list:
-                            interaction_result, interaction, route_time = yield from self._create_and_score_interaction(timeline, aop_data.aop, aop_data.inventory_type, best_threshold)
-                            if not interaction_result:
-                                if self._request.record_test_result is not None:
-                                    self._request.record_test_result(aop_data.aop, '_create_and_score_interaction', interaction_result)
-                                if self._gsi_interactions is not None:
-                                    self._gsi_interactions.append(interaction_result)
-                                continue
-                            _, best_threshold = self._process_scored_interaction(aop_data.aop, interaction, interaction_result, route_time, best_threshold)
-
-                self._limited_affordances.clear()
-                if not motives_to_score:
-                    break
-                    
-                with Timer("Statistics to Score"):
-
-                    self._formerly_scored_motives.update(self._actively_scored_motives)
-                    variance_score = self._motive_scores[motives_to_score[0]]
-                    for motive in self._found_motives:
-                        variance_score = max(variance_score, self._motive_scores[motive])
-
-                    variance_score *= AutonomyMode.FULL_AUTONOMY_STATISTIC_SCORE_VARIANCE
-                    self._actively_scored_motives = {stat.stat_type for stat in itertools.takewhile(lambda desire: self._motive_scores[desire] >= variance_score, motives_to_score)}
-                    if not self._actively_scored_motives:
-                        break
-                    if self._found_valid_interaction:
-                        motives_to_score = []
-                    else:
-                        motives_to_score = motives_to_score[len(self._actively_scored_motives):]
-
-            final_valid_interactions = None
-            for i in AutonomyInteractionPriority:
-                if i not in self._valid_interactions:
-                    continue
-                valid_interactions = self._valid_interactions[i]
-                if valid_interactions:
-                    final_valid_interactions = valid_interactions
-                    break
-
-            self._request.valid_interactions = final_valid_interactions
-            if self._gsi_interactions is not None:
-                self._request.gsi_data = {GSIDataKeys.COMMODITIES_KEY: self._motive_scores.values(),  GSIDataKeys.AFFORDANCE_KEY: self._gsi_interactions,  GSIDataKeys.PROBABILITY_KEY: [],  GSIDataKeys.OBJECTS_KEY: self._gsi_objects, 
-                 GSIDataKeys.MIXER_PROVIDER_KEY: None, 
-                 GSIDataKeys.MIXERS_KEY: [],  GSIDataKeys.REQUEST_KEY: self._request.get_gsi_data()}
-            #ts4mp_log("FullAutonomy", str(final_valid_interactions))
-            #ts4mp_log("potential", "end")
-            return final_valid_interactions    
-    except Exception as e:
-        ts4mp_log("FullAutonomy", str(e))
-        ts4mp_log("FullAutonomy", 'Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-
-import objects.script_object 
-from carry.carry_utils import get_carried_objects_gen
-
-def potential_interactions(self, context, get_interaction_parameters=None, allow_forwarding=True, **kwargs):
-    try:
-        count = 0
-        aops = []
-        for affordance in self.super_affordances(context):
-            if not self.supports_affordance(affordance):
-                pass
-            if get_interaction_parameters is not None:
-                interaction_parameters = get_interaction_parameters(affordance, kwargs)
-            else:
-                interaction_parameters = kwargs
-            for aop in affordance.potential_interactions(self, context, **interaction_parameters):
-                count += 1
-                aops.append(aop)
-                yield aop
-        #ts4mp_log("potential", "{} has: {} affordances: {}".format(self, count, aops))
-        if allow_forwarding and self.allow_aop_forward():
-            for aop in self._search_forwarded_interactions(context, get_interaction_parameters=get_interaction_parameters, **kwargs):
-                yield aop
-        if self.parent is not None:
-            yield from self.parent.child_provided_aops_gen(self, context, **kwargs)
-        club_service = services.get_club_service()
-        if club_service is not None:
-            for (club, affordance) in club_service.provided_clubs_and_interactions_gen(context):
-                aop = AffordanceObjectPair(affordance, self, affordance, None, associated_club=club, **kwargs)
-                if aop.test(context):
-                    yield aop
-        context_sim = context.sim
-        if context_sim is not None and context_sim.posture_state is not None:
-            for (_, _, carried_object) in get_carried_objects_gen(context_sim):
-                yield from carried_object.get_provided_aops_gen(self, context, **kwargs)
-    except Exception as e:
-        ts4mp_log("potential", e)
-        
-        
-objects.script_object.ScriptObject.potential_interactions = potential_interactions
 autonomy.autonomy_service.AutonomyService._update_gen = _update_gen
-autonomy.autonomy_modes.AutonomyMode.run_gen = run_gen
-autonomy.autonomy_modes.FullAutonomy._run_gen = _run_gen
